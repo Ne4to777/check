bb1ce5ec48936716fdfd624891908ddbef1b6107
Alexey Alexeyev <ne4to777@gmail.com>
2020-12-19 21:26:33 +0300

    fix

--- src/utils/event/index.ts
+++ src/utils/event/index.ts
@@ -1,5 +1,6 @@
 import type {AnyToAnyT} from '../types/functions';
 import {B, C} from '../../utils/combinators';
+
 const persistEvent: AnyToAnyT = e => e.persist() || e;
 const composeWithPersist = C(B)(persistEvent);
 
